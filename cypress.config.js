b5317b7d704812dc6cdbdc70538728b7c5090d4c
Sushant-2106 <sushantmishra2110@gmail.com>
2024-09-28 18:43:48 +0530

    Cypress Learning

--- cypress.config.js
+++ cypress.config.js
@@ -14,4 +14,12 @@ module.exports = defineConfig({
     specPattern:"cypress/UAT/Features/*.feature"
     
   },
+  reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports/mochawesome-report',
+      overwrite: false,
+      html: true,
+      json: true,
+      charts: true
+    }
 });
